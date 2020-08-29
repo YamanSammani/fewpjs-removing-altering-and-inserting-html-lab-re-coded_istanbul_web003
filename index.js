2bba5cc0dd090b3c8f634da85725e4b8bfb85e09
YamanSammani <yaman.mys@gmail.com>
2020-08-29 06:18:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+// Write your code here
+let main = document.querySelector("main#main");
+main.remove();
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+let text = document.createTextNode("victory");
+newHeader.appendChild(text);
+
+newHeader.innerHTML = "<h1>victory</h1><h3>Yaman is the champion </h3>";
+
+
+
+
+// let elemnt = document.getElementById("new");
+// elemnt.appendChild(newHeader);
+
+
